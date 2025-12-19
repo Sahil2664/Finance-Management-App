2b99a539da0f3b4663cf81732a896bb8673d4857
Sahil Kumar Singh <152484304+Sahil2664@users.noreply.github.com>
2025-12-19 13:26:51 +0530

    Little changes

--- src/main/java/com/financeapp/Main.java
+++ src/main/java/com/financeapp/Main.java
@@ -1,7 +1,11 @@
 package com.financeapp;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        new LoginFrame();
+        SwingUtilities.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
     }
 }
